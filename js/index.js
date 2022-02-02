13e94665c69ee7e8d20e14cb8a6c507fb37789cc
Aadesh_Tendolkar <Aadesh_Tendolkar>
2022-02-02 16:59:18 +0530

    First Level (Front end done) TODO-: Complete JS functions of calci

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const span = document.getElementById('result')
+const equals = document.getElementById('equals')
+
+
+equals.addEventListener('click', () => {
+    // console.log("Click")
+    span.innerText = 'Click'
+})
